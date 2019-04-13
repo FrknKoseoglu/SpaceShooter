8a4b23adf32a38c0d640b6c586d00985d781e646
Furkan Köseoğlu <furkan1771@gmail.com>
2019-04-14 00:56:36 +0300

    Random Asteroid
    Random Asteroid oluşturma

---
+++ SpaceShooter/Assets/Code/OyunKontrol.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OyunKontrol : MonoBehaviour
+{
+    public GameObject Asteroid;
+    public Vector3 randomPos;
+    void Start()
+    {
+        olustur(); 
+    }
+    void olustur()
+    {
+        Vector3 vec = new Vector3(Random.Range(-randomPos.x,randomPos.x),0,randomPos.z);
+        Instantiate (Asteroid,vec,Quaternion.identity);
+    }
+
+   
+}
