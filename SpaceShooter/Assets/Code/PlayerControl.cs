5a823e88fb3d2f194d192ff497692f1d3a6e18a3
Furkan Köseoğlu <furkan1771@gmail.com>
2019-04-13 16:52:16 +0300

    Hareket
    Sağ sol ileri geri hareketi oluşturuldu.
    sağa sola eğim verildi.
    hareket alanı sınırlandırıldı.

---
+++ SpaceShooter/Assets/Code/PlayerControl.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    Rigidbody fizik;
+    float horizontal = 0;
+    float vertical = 0;
+    Vector3 vec;
+    public float karakterHiz;
+    public float minX,maxX,minZ,maxZ,egim;
+
+    void Start()
+    {
+        fizik = GetComponent<Rigidbody>();
+    }
+
+
+    void FixedUpdate()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        vec = new Vector3(horizontal,0,vertical);
+
+        fizik.velocity = vec*karakterHiz;
+
+        fizik.position = new Vector3
+         (
+            Mathf.Clamp(fizik.position.x,minX,maxX),
+            0.0f,
+            Mathf.Clamp(fizik.position.z,minZ,maxZ)
+        );
+
+        fizik.rotation = Quaternion.Euler(0, 0, fizik.velocity.x*-egim);
+
+
+        
+
+    }
+}
