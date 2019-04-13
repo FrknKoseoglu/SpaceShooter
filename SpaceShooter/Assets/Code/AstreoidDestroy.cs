5d38cebc3ade5fe437eb0b025bdc4a48101d5c1f
Furkan Köseoğlu <furkan1771@gmail.com>
2019-04-14 00:24:03 +0300

    Astreoid Yapıldı
    Astreoid Yapıldı ateş edince yok oluyor.

---
+++ SpaceShooter/Assets/Code/AstreoidDestroy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AstreoidDestroy : MonoBehaviour
+{
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag != "sinir")
+        {
+            Destroy(col.gameObject);
+            Destroy(gameObject);
+        }
+
+    }
+}
