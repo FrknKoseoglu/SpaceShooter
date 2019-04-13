e45c1f0fbfae1aa8a8ffccc4a467082ad4ebdbb7
Furkan Köseoğlu <furkan1771@gmail.com>
2019-04-14 00:41:05 +0300

    Patlama Efekti
    Astreoid ve Player Çarpma Efekti Eklendi.

---
+++ SpaceShooter/Assets/Code/AstreoidCarpma.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AstreoidCarpma : MonoBehaviour
+{
+    public GameObject patlama;
+    public GameObject playerpatlama;
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag != "sinir")
+        {
+            Destroy(col.gameObject);
+            Destroy(gameObject);
+            Instantiate(patlama, transform.position, transform.rotation);
+           
+        }
+
+        if (col.tag == "Player")
+        {
+            Instantiate(playerpatlama, col.transform.position, col.transform.rotation);
+        }
+
+    }
+}
